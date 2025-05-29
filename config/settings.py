f12da79ca8c79c4b959ed59ef6f8d4b2f5ff61cc
Alexey Solovyev <solovaa@yandex.ru>
2025-05-29 23:03:04 +0000

    add some settings

--- config/settings.py
+++ config/settings.py
@@ -112,10 +112,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / 'static/',)

c3207cbee95b8cc0acc9b8712b4b22060be847c7
Alexey Solovyev <solovaa@yandex.ru>
2025-06-18 20:05:46 +0000

    add CRUD for BlogPost model

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import BlogPost
 
-# Create your views here.
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blogpost_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(is_published=True)
+        return queryset
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blogpost_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        """Увеличиваем счетчик просмотров при каждом открытии записи."""
+        obj = super().get_object(queryset)
+        obj.views_count += 1
+        obj.save()
+        return obj
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/blogpost_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blog/blogpost_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/blogpost_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')

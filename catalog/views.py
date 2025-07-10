47742d8d97c56e7c3b45b9883e9af7c1f9bb9ed8
Alexey Solovyev <solovaa@yandex.ru>
2025-07-10 20:13:46 +0000

    add url for unpublish and modified views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 from django.urls import reverse_lazy
 from .models import Product
@@ -35,3 +37,17 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.has_perm('catalog.delete_product'):
+            return self.handle_no_permission()
+        return super().dispatch(request, *args, **kwargs)
+
+@permission_required('catalog.can_unpublish_product')
+def unpublish_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        product.is_published = False
+        product.save()
+        return redirect('catalog:product_detail', pk=product.pk)
+    return render(request, 'catalog/confirm_unpublish.html', {'product': product})
